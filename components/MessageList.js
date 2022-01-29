226238459fd877a51a7158f81c6a778e1e3db94e
Carolyne Lira <carolyne.lira@meliuz.com.br>
2022-01-28 22:10:37 -0300

    Adiciona botão de enviar e excluir mensagens

--- components/MessageList.js
+++ components/MessageList.js
@@ -1,8 +1,7 @@
 import appConfig from '../config.json';
-import { Box, Text, Image } from '@skynexui/components';
+import { Box, Text, Image, Button } from '@skynexui/components';
 
 export function MessageList(props) {
-    console.log(props.messages);
     return (
         <Box
             tag="ul"
@@ -17,51 +16,79 @@ export function MessageList(props) {
         >
             {props.messages.map((message) => {
                 return (
-                    <Text
-                        key={message.id}
-                        tag="li"
+                    <Box
                         styleSheet={{
-                            borderRadius: '5px',
-                            padding: '6px',
-                            marginBottom: '12px',
-                            hover: {
-                                backgroundColor: appConfig.theme.colors.neutrals[700],
-                            }
+                            display: 'flex',
                         }}
                     >
-                        <Box
+                        <Text
+                            key={message.id}
+                            tag="li"
+                            style={{
+                                width: '98.5%',
+                            }}
                             styleSheet={{
-                                marginBottom: '8px',
+                                borderRadius: '5px',
+                                padding: '6px',
+                                marginBottom: '12px',
+                                backgroundColor: appConfig.theme.colors.neutrals[700]
                             }}
                         >
-                            <Image
+                            <Box
                                 styleSheet={{
-                                    width: '20px',
-                                    height: '20px',
-                                    borderRadius: '50%',
-                                    display: 'inline-block',
-                                    marginRight: '8px',
+                                    marginBottom: '8px',
                                 }}
-                                src={`https://github.com/carollira.png`}
-                            />
-                            <Text tag="strong">
-                                {message.user}
-                            </Text>
-                            <Text
-                                styleSheet={{
-                                    fontSize: '10px',
-                                    marginLeft: '8px',
-                                    color: appConfig.theme.colors.neutrals[300],
-                                }}
-                                tag="span"
                             >
-                                {(new Date().toLocaleDateString())}
-                            </Text>
-                        </Box>
-                        {message.text}
-                    </Text>
+                                <Image
+                                    styleSheet={{
+                                        width: '20px',
+                                        height: '20px',
+                                        borderRadius: '50%',
+                                        display: 'inline-block',
+                                        marginRight: '8px',
+                                    }}
+                                    src={`https://github.com/carollira.png`}
+                                />
+                                <Text tag="strong">
+                                    {message.user}
+                                </Text>
+                                <Text
+                                    styleSheet={{
+                                        fontSize: '10px',
+                                        marginLeft: '8px',
+                                        color: appConfig.theme.colors.neutrals[300],
+                                    }}
+                                    tag="span"
+                                >
+                                    {(new Date().toLocaleDateString())}
+                                </Text>
+                            </Box>
+                            {message.text}
+                        </Text>
+                        <Button
+                            iconName='times'
+                            onClick={() => props.handleRemoveMessage(message.id)}
+                            style={{
+                                fontSize: '12px',
+                                height: '18px',
+                                width: '18px',
+                            }}
+                            styleSheet={{
+                                marginLeft: '-8px',
+                                marginTop: '-8px',
+                                backgroundColor: appConfig.theme.colors.neutrals[700],
+                                hover: {
+                                    backgroundColor: appConfig.theme.colors.primary[700]
+                                },
+                                focus: {
+                                    backgroundColor: appConfig.theme.colors.primary[700]
+                                }
+                            }}
+                        >
+                        </Button>
+                    </Box>
                 );
             })}
-        </Box>
+        </Box >
     )
 }
