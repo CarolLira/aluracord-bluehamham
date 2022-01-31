b69d2247d2a1e0dc267bb5ae818c31da19f80886
Carolyne Lira <carolyne.lira@meliuz.com.br>
2022-01-31 16:10:13 -0300

    feat: Adiciona conexão com Supabase
    - Busca mensagens no banco de dados
    - Salva novas mensagens digitas no banco de dados

--- pages/chat.js
+++ pages/chat.js
@@ -1,28 +1,53 @@
 import { Box, Button, TextField } from '@skynexui/components';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import appConfig from '../config.json';
 
 import { Header } from '../components/Header';
 import { MessageList } from '../components/MessageList';
+import { createClient } from '@supabase/supabase-js';
+
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTY0MzU0NzM1NSwiZXhwIjoxOTU5MTIzMzU1fQ.-HN_qLXIyr2hS05qgKh58Q5uZlUbaOHs1ACbuFj9hvg';
+const SUPABASE_URL = 'https://tvmgdwwnhcuaukrsvrxl.supabase.co';
+
+const supabaseClient = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 export default function ChatPage() {
     const [message, setMessage] = useState('');
     const [messageHistory, setMessageHistory] = useState([]);
 
+    useEffect(() => {
+        supabaseClient
+            .from('messages')
+            .select('*')
+            .order('id', { ascending: false })
+            .then(({ data }) => {
+                console.log('Dados da consulta:', data);
+                setMessageHistory(data);
+            });
+    }, []);
+
     function handleNewMessage(newMessage) {
         const messageData = {
-            id: messageHistory.length + 1,
+            // id: messageHistory.length + 1,
             user: 'carollira',
             text: newMessage,
         }
 
         if (newMessage) {
-            setMessageHistory([
-                messageData,
-                ...messageHistory
-
-            ]);
+            supabaseClient
+                .from('messages')
+                .insert([
+                    messageData
+                ])
+                .then((response) => {
+                    console.log('criando msg: ', response);
+                    setMessageHistory([
+                        response.data[0],
+                        ...messageHistory,
+                    ]);
+                });
         }
+
         setMessage('');
     }
 
